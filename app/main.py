33c0b84383684a673f4f55317a2e781fbccdff0e
matteo-zirilli <matteo.zirilli@virgilio.it>
2026-03-17 13:34:09 +0000

    Change HTML Response 2

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ analyzer = SentimentModel()
 @app.get("/")
 def root():
     content = """<h1>WELCOME!!Here you can test any text or chat or reviews to classify as Positive, Neutral or Negative</h1>
-                <a href="/docs">Clicca qui per testare la API</a>"""
+                <a href="/docs">Click here to test the API</a>"""
     return HTMLResponse(content = content)
 
 #poiché devo inviare dei dati al server (un testo) creo un endpoint di post
